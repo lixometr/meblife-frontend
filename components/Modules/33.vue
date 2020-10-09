eae2e94220149fa9bfe2ca0f62e8e89222eab439
Yarik <lixometr@mail.ru>
2020-10-09 17:38:43 +0300

    fix

---
+++ components/Modules/33.vue
@@ -0,0 +1,25 @@
+<template>
+  <Module id="33">
+    <h2 class="text-24 font-bold uppercase text-left pb-3" v-if="title">{{title}}</h2>
+    <div class="mt-3" v-if="subTitle">{{subTitle}}</div>
+    <div class="module-33__grid flex flex-wrap md-column" v-if="moduleImages.length > 0">
+      <nuxt-link class="module-33__item pr-3" v-for="(item, idx) in moduleImages" :key="idx">
+        <div>{{item.title}}</div>
+      </nuxt-link>
+    </div>
+  </Module>
+</template>
+
+<script>
+import moduleValidator from "./moduleValidator";
+import ModuleMixin from "./ModuleMixin";
+export default {
+  props: {
+    item: moduleValidator,
+  },
+  mixins: [ModuleMixin],
+};
+</script>
+
+<style lang="scss">
+</style>
